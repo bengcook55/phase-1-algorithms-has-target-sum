11c6c4b5cc143e68fd71952d91305dd1d4572280
Benjamin Cook <bengcook55@gmail.com>
2022-12-08 21:39:42 -0500

    commit tree dd5b07dfe44c21362e7a48ed4cf4270be870acce

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
-
+    for (let i = 0; i < array.length; i++) {
+      const complement = target - array[i];
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] === complement) return true;
+      }
+    }
+    return false;
+  }
 /* 
   Write the Big O time complexity of your function here
+
+  this would be O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  initialize i at zero, long as its lets than array length incrment one 
+  allow complement to be the difference b/w target and array
+  
 */
 
 /*
